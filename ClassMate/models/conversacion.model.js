875d4756c439108842f072aeb7d4e0bf4173d9b8
miky3000 <mikecode808@gmail.com>
2023-04-15 18:29:16 +0200

    modificada query getMensajeByConversacionId

--- ClassMate/models/conversacion.model.js
+++ ClassMate/models/conversacion.model.js
@@ -17,7 +17,10 @@ const InsertMessage = ({ contenido, emisor_id, receptor_id, conversaciones_id, f
 
 // Recuperar mensajes por conversación
 const getMensajeByConversacionId = (conversacionId) => {
-    return db.query('SELECT * FROM escuelabeta_definitivo.mensaje WHERE conversaciones_id = ?;', [conversacionId])
+    return db.query(`SELECT u2.nombre as 'emisor',u.nombre as 'receptor',m.* FROM escuelabeta_definitivo.mensaje as m 
+    join escuelabeta_definitivo.usuarios as u on u.id=m.receptor_id
+    join escuelabeta_definitivo.usuarios as u2 on u2.id=m.emisor_id
+    where m.conversaciones_id=?;`, [conversacionId])
 }
 
 // Recuperar Conversaciones
