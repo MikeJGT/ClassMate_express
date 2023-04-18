12850d8aac0b753cf655a53b54103653b82d78b7
miky3000 <mikecode808@gmail.com>
2023-04-18 16:17:51 +0200

    creando modelo clases

--- ClassMate/models/clase.model.js
+++ ClassMate/models/clase.model.js
@@ -1,3 +1,8 @@
+const getAllClases = () => {
+
+}
+
+
 const getClaseByAlumnoID = (alumId) => {
     return db.query(`SELECT alum.*  FROM escuelabeta_definitivo.alumnos_has_clases  as  alum
     JOIN escuelabeta_definitivo.usuarios as u on u.id = alum.alumno_id
